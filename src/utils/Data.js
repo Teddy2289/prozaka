06cc6183dd2cac183f216cf552bad43a61f636e1
teddy <teddyrinah@gmail.com>
2023-05-21 20:55:28 +0200

    second  commit

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -19,7 +19,7 @@ export const ProjetDetail = [
         projet_name: "Immobillier",
         projet_description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
         projet_img: Project1,
-        tect_stack: ['Reactjs', 'TailwindCss'],
+        tect_stack: ['Reactjs', 'CSS/SCSS'],
         project_url: "www.google.com",
         reverse: false
     },
