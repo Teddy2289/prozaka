0626d6319fc6587616b45ef7582262afc7d4baf1
teddy <teddyrinah@gmail.com>
2023-05-21 20:56:21 +0200

    second  commit

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -20,7 +20,7 @@ export const ProjetDetail = [
         projet_description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
         projet_img: Project1,
         tect_stack: ['Reactjs', 'CSS/SCSS'],
-        project_url: "www.google.com",
+        project_url: "https://immobilierteddy.netlify.app/",
         reverse: false
     },
     {
