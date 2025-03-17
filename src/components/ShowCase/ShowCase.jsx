d036f1fccfe14b71ff9a3357ea7dd14e1d4a3354
Teddy <teddyrinah@gmail.com>
2025-03-17 12:23:54 +0300

    change picture

--- src/components/ShowCase/ShowCase.jsx
+++ src/components/ShowCase/ShowCase.jsx
@@ -1,126 +1,131 @@
 import React from "react";
 import {
-    Heading,
-    PaddingContainner,
-    ParaText,
-    FlexContainer,
-    BlueText,
-    IconContainer,
+  Heading,
+  PaddingContainner,
+  ParaText,
+  FlexContainer,
+  BlueText,
+  IconContainer,
 } from "../../styled/Global.styled";
-import {BsLinkedin, BsFacebook, BsInstagram} from "react-icons/bs";
+import { BsLinkedin, BsFacebook, BsInstagram } from "react-icons/bs";
 
 import {
-    ShowCaseImageCard,
-    ShowCaseParticleContainer,
-    Particle,
+  ShowCaseImageCard,
+  ShowCaseParticleContainer,
+  Particle,
 } from "../../styled/ShowCase.styled/ShowCase.styled";
 
 import teddy from "../../assets/teddy.jpg";
 import background1 from "../../assets/triangle.png";
 import background from "../../assets/technologique.png";
-import {motion} from "framer-motion";
-import {fadeInLeftVariant, fadeInRightVariant} from "../../utils/Variant";
+import { motion } from "framer-motion";
+import { fadeInLeftVariant, fadeInRightVariant } from "../../utils/Variant";
 
 function ShowCase() {
-    return (
-        <PaddingContainner id="Home" left="3%" right="10%" top="18%" bottom="10%" responsiveLeft="1rem"
-                           responsiveRight="1rem" responsiveTop="8rem">
-            <FlexContainer align="start" fullWidthChild>
-                <motion.div
-                    variants={fadeInLeftVariant}
-                    initial="hidden"
-                    whileInView="visible"
-                >
-                    <Heading as="h4" size="h4">
-                        Hello
-                    </Heading>
-                    <Heading as="h2" size="h2" top="0.5rem" bottom="1rem">
-                        I'm <BlueText>Teddy Rinah</BlueText>
-                    </Heading>
-                    <Heading as="h3" size="h3" top="0.5rem" bottom="1rem">
-                        I'm a <BlueText>Developers</BlueText>
-                    </Heading>
-                    <ParaText as="p" top="2rem" bottom="4rem">
-                        I'm a developer web with 2 years of experience in creating and
-                        designing user-friendly websites and web application
-                    </ParaText>
-                    <FlexContainer gap="20px" responsiveFlex>
-                        <IconContainer color="white" size="1.5rem">
-                            <BsLinkedin/>
-                        </IconContainer>
-                        <IconContainer color="white" size="1.5rem">
-                            <BsFacebook/>
-                        </IconContainer>
-                        <IconContainer color="white" size="1.5rem">
-                            <BsInstagram/>
-                        </IconContainer>
-                    </FlexContainer>
-                </motion.div>
+  return (
+    <PaddingContainner
+      id="Home"
+      left="3%"
+      right="10%"
+      top="18%"
+      bottom="10%"
+      responsiveLeft="1rem"
+      responsiveRight="1rem"
+      responsiveTop="8rem">
+      <FlexContainer align="start" fullWidthChild>
+        <motion.div
+          variants={fadeInLeftVariant}
+          initial="hidden"
+          whileInView="visible">
+          <Heading as="h4" size="h4">
+            Hello
+          </Heading>
+          <Heading as="h2" size="h2" top="0.5rem" bottom="1rem">
+            I'm <BlueText>Teddy Rinah</BlueText>
+          </Heading>
+          <Heading as="h3" size="h3" top="0.5rem" bottom="1rem">
+            I'm a <BlueText>Developers</BlueText>
+          </Heading>
+          <ParaText as="p" top="2rem" bottom="4rem">
+            I'm a developer web with 2 years of experience in creating and
+            designing user-friendly websites and web application
+          </ParaText>
+          <FlexContainer gap="20px" responsiveFlex>
+            <IconContainer color="white" size="1.5rem">
+              <BsLinkedin />
+            </IconContainer>
+            <IconContainer color="white" size="1.5rem">
+              <BsFacebook />
+            </IconContainer>
+            <IconContainer color="white" size="1.5rem">
+              <BsInstagram />
+            </IconContainer>
+          </FlexContainer>
+        </motion.div>
 
-                <FlexContainer
-                    as={motion.div}
-                    variants={fadeInRightVariant}
-                    initial="hidden"
-                    whileInView="visible"
-
-                    justify="flex-end">
-                    <ShowCaseParticleContainer>
-                        <ShowCaseImageCard>
-                            <img src={teddy} width={300} alt="teddy"/>
-                        </ShowCaseImageCard>
-                        <Particle
-                            as={motion.img}
-                            animate={{
-                                x: [0, 100, 0],
-                                rotate: 360,
-                                scale: [1, 0.5, 1]
-                            }}
-                            transition={{
-                                duration: 20,
-                                repeat: Infinity
-                            }}
-                            src={background}
-                            top="-80px"
-                            left="20px"
-                            rotate="60deg"
-                        />
-                        <Particle
-                            as={motion.img}
-                            animate={{
-                                y: [0, 100, 0],
-                                rotate: 360,
-                                scale: [1, 0.8, 1]
-                            }}
-                            transition={{
-                                duration: 18,
-                                repeat: Infinity
-                            }}
-                            src={background1}
-                            top="50px"
-                            right="-70px"
-                            rotate="0deg"
-                        />
-                        <Particle
-                            as={motion.img}
-                            animate={{
-                                y: [0, -100, 0],
-                                rotate: 360,
-                                scale: [1, 0.9, 1]
-                            }}
-                            transition={{
-                                duration: 15,
-                                repeat: Infinity
-                            }}
-                            src={background}
-                            bottom="10px"
-                            left="-70px"
-                            rotate="50deg"
-                        />
-                    </ShowCaseParticleContainer>
-                </FlexContainer>
-            </FlexContainer>
-        </PaddingContainner>
-    );
+        <FlexContainer
+          as={motion.div}
+          variants={fadeInRightVariant}
+          initial="hidden"
+          whileInView="visible"
+          justify="flex-end">
+          <ShowCaseParticleContainer>
+            <ShowCaseImageCard>
+              <img src={teddy} alt="teddy" />
+            </ShowCaseImageCard>
+            <Particle
+              as={motion.img}
+              animate={{
+                x: [0, 100, 0],
+                rotate: 360,
+                scale: [1, 0.5, 1],
+              }}
+              transition={{
+                duration: 20,
+                repeat: Infinity,
+              }}
+              src={background}
+              top="-80px"
+              left="20px"
+              rotate="60deg"
+            />
+            <Particle
+              as={motion.img}
+              animate={{
+                y: [0, 100, 0],
+                rotate: 360,
+                scale: [1, 0.8, 1],
+              }}
+              transition={{
+                duration: 18,
+                repeat: Infinity,
+              }}
+              src={background1}
+              top="50px"
+              right="-70px"
+              rotate="0deg"
+            />
+            <Particle
+              as={motion.img}
+              animate={{
+                y: [0, -100, 0],
+                rotate: 360,
+                scale: [1, 0.9, 1],
+              }}
+              transition={{
+                duration: 15,
+                repeat: Infinity,
+              }}
+              src={background}
+              bottom="10px"
+              left="-70px"
+              rotate="50deg"
+            />
+          </ShowCaseParticleContainer>
+        </FlexContainer>
+      </FlexContainer>
+    </PaddingContainner>
+  );
 }
 
 export default ShowCase;
--- src/styled/ShowCase.styled/ShowCase.styled.js
+++ src/styled/ShowCase.styled/ShowCase.styled.js
@@ -2,25 +2,30 @@ import { styled } from "styled-components";
 
 export const ShowCaseParticleContainer = styled.div`
   position: relative;
-   @media (max-width: ${({theme}) => theme.breackpoints.mobile}) {
+  @media (max-inline-size: ${({ theme }) => theme.breackpoints.mobile}) {
     display: none;
-     
   }
 `;
 export const ShowCaseImageCard = styled.div`
-  border: 1px solid white;
-  width: max-content;
-  padding-top: 2rem;
-  border-radius: 1rem;
+  inline-size: 500px;
+  block-size: 500px;
+  object-fit: cover;
   position: relative;
   overflow: hidden;
+  border-radius: 50%;
+  padding-block-end: 2rem;
+
+  img {
+    inline-size: 100%;
+    object-fit: cover;
+  }
 `;
 export const Particle = styled.img`
   position: absolute;
-  top: ${({ top }) => top};
-  left: ${({ left }) => left};
-  right: ${({ right }) => right};
-  bottom: ${({ bottom }) => bottom};
-  width: 60px;
+  inset-block-start: ${({ top }) => top};
+  inset-inline-start: ${({ left }) => left};
+  inset-inline-end: ${({ right }) => right};
+  inset-block-end: ${({ bottom }) => bottom};
+  inline-size: 60px;
   transform: rotate(${({ rotate }) => rotate});
 `;
